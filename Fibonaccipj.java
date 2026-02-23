50a72afeb2bc626b99dde0055f13028c9a1865a2
Samiksha Purohit <samikshapurohit91@gmail.com>
2026-02-23 12:05:27 +0530

    Add files via upload

---
+++ Fibonaccipj.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Fibonaccipj{
+	public static void main(String [] s){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number upto you want Fibonacci series : ");
+		int dn = sc.nextInt();
+
+		int n1= 0;
+		int n2= 1;
+
+		for(int i=1; i<=dn;i++){
+
+			System.out.print(n1 + " , ");
+			int n3= n1+n2;
+			n1 = n2;
+			n2= n3;
+
+
+		}
+	}
+}
+

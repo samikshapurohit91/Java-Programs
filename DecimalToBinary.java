d2d385cf6b2849a479c1b0026768875bf79ed2ac
Samiksha Purohit <samikshapurohit91@gmail.com>
2026-03-03 12:33:30 +0530

    Add files via upload

---
+++ DecimalToBinary.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class DecimalToBinary
+{
+
+    // function to convert decimal to binary
+    static void decToBinary(int n)
+    {
+        // array to store binary number
+        int[] binaryNum = new int[1000];
+
+        // counter for binary array
+        int i = 0;
+        while (n > 0)
+        {
+            // storing remainder in binary array
+            binaryNum[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+
+        // printing binary array in reverse order
+        for (int j = i - 1; j >= 0; j--)
+            System.out.print(binaryNum[j]);
+    }
+
+
+    public static void main (String[] args)
+    {
+
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Decimal - " );
+      int n = sc.nextInt();
+
+        System.out.print("Binary - ");
+          decToBinary(n);
+	  }
+
+}

b7b031c82d669de61585bfc8978dbe6e035663d2
Samiksha Purohit <samikshapurohit91@gmail.com>
2026-04-07 13:26:46 +0530

    Add files via upload

---
+++ Star0.java
@@ -0,0 +1,13 @@
+class Star0{
+ public static void main(String [] args ){
+
+ 	int n= 5;
+ 	for(int i=1; i<=n; i++){
+ 		if(i % 2 != 0){
+ 		System.out.println(" *");
+ 		}else{
+ 		System.out.println("* * ");
+ 		}
+	}
+ 	}
+ }
